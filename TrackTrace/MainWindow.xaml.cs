61845480b619d67f6555580ddfa69c1aacaa2b91
Klaudia <56263298+Zireael-K@users.noreply.github.com>
2020-11-11 12:16:05 +0000

    Add project files.

---
+++ TrackTrace/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackTrace
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddLocationBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RecordContactBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RecordVisitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchContactsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchLocationBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
